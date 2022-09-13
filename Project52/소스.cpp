8d5594aa0e6f6836dee0bd4c546345865d0dd044
이민지 <dbstnalswl123@naver.com>
2022-09-13 23:48:21 +0900

    2

--- Project52/소스.cpp
+++ Project52/소스.cpp
@@ -1,38 +1,70 @@
 #include <iostream>
+#include <stdlib.h>
+#include <random>
 using namespace std;
 
 int main()
 {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dis(1, 10);
+
+	int i, answer;
 	char ch;
-	int vowel = 0, consonant = 0;
-	cout << "영문자를 입력하고 ctrl+z를 치세요" << endl;
+	cout << "산수 문제를 자동으로 출제합니다." << endl;
 
-	while (cin >> ch)
+	while (true)
 	{
-		if (ch >= 'A' && ch <= 'Z')
+		int x = dis(gen);
+		int y = dis(gen);
+
+		cout << x << " " << y << "\n";
+		cout << "연산자 입력하기" << endl;
+		cin >> ch;
+		cout << "결과 입력하기" << endl;
+		cin >> answer;
+
+		if (ch == '+')
 		{
-			cout << "Check to alphabet" << endl;
-			consonant--;
+			if (x + y == answer)
+			{
+				cout << "맞았습니다." << endl;
+				break;
+			}
+			else
+				cout << "틀렸습니다." << endl;
+		}
+		else if (ch == '-')
+		{
+			if (x - y == answer)
+			{
+				cout << "맞았습니다." << endl;
+				break;
+			}
+			else
+				cout << "틀렸습니다." << endl;
+		}
+		else if (ch == '/')
+		{
+			if (x / y == answer)
+			{
+				cout << "맞았습니다." << endl;
+				break;
+			}
+			else
+				cout << "틀렸습니다." << endl;
+		}
+		else if (ch == '%')
+		{
+			if (x % y == answer)
+			{
+				cout << "맞았습니다." << endl;
+				break;
+			}
+			else
+				cout << "틀렸습니다." << endl;
 		}
 
-		switch (ch)
-		{
-		case 'a':
-		case 'i':
-		case 'e':
-		case 'o':
-		case 'u':
-			vowel++;
-			break;
-
-		default:
-			consonant++;
-			break;
-		}
 	}
-
-	cout << "자음 갯수 : " << consonant << endl;
-	cout << "모음 갯수 : " << vowel << endl;
-
 	return 0;
 }
